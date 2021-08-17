8f085ff10370ee93fbe1adabf7e2302fb3caae0d
Phoenix <phoenixrabang@gmail.com>
2021-08-17 11:29:33 -0700

    second days commit

--- src/polybius.js
+++ src/polybius.js
@@ -4,16 +4,85 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-    // you can add any code you want within this function scope
-  
-    function polybius(input, encode = true) {
-      // your solution code here
+  //hardcoded alphabet and cipher key:value pairs
+// you can add any code you want within this function scope
+  const cipher = {
+    11: 'a',
+    21: 'b',
+    31: 'c',
+    41: 'd',
+    51: 'e',
+    12: 'f',
+    22: 'g',
+    32: 'h',
+    42: 'i/j',
+    52: 'k',
+    13: 'l',
+    23: 'm',
+    33: 'n',
+    43: 'o',
+    53: 'p',
+    14: 'q',
+    24: 'r',
+    34: 's',
+    44: 't',
+    54: 'u',
+    15: 'v',
+    25: 'w',
+    35: 'x',
+    45: 'y',
+    55: 'z'
+  }
+
+  const grid = Object.entries(cipher)
+  grid.push([42, 'i'])
+  grid.push([42, 'j'])
+
+
+  function polybius(input, encode = true) {
+    input = input.toLowerCase()
+    // your solution code here
+    const nonSpaceChars = input.split(' ').reduce((acc, ele) => acc += ele.length, 0)
+    //declare return variable for encoding
+    let encodedMsg = ''
+    //if encode is false, input.length must be even
+    if (!encode && nonSpaceChars % 2 != 0) {
+      return false
+    } else if (!encode) {
+      let decodedMsg = ''
+      //declare return variable for decoding
+      for (const char of input) {
+        //append spaces
+        if (char === ' ') {
+          encodedMsg += char
+          //decode numbers
+        } else if (decodedMsg.length > 0) {
+          decodedMsg += char
+          encodedMsg += cipher[parseInt(decodedMsg)]
+          decodedMsg = ''
+        } else {
+          decodedMsg += char
+        }
+      }
+      return encodedMsg
+    } else {
+      //encode letters
+      for (char of input) {
+        if (char === ' ') {
+          encodedMsg += char
+          //keep goin
+          continue
+        }
+        let encoded = grid.filter(subArr => subArr.includes(char))[0]
+        encodedMsg += encoded[0]
+      }
+      return encodedMsg
     }
-  
-    return {
-      polybius,
-    };
-  })();
-  
-  module.exports = { polybius: polybiusModule.polybius };
-  
+  }
+
+  return {
+    polybius,
+  };
+})();
+
+module.exports = { polybius: polybiusModule.polybius };
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,36 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const {polybius} = require("../src/polybius");
+
+
+describe("polybius", () => {
+  it("should encode each letter to corresponding number pair", () =>{
+    const input = "bugs bunny";
+    const encode = true;
+    const actual = polybius(input, encode);
+    const expected = "21542234 2154333345";
+    expect(actual).to.eql(expected);
+  })
+  it("should output a string", () =>{
+    const input = "bugs bunny";
+    const encode = true;
+    const actual = polybius(input, encode);
+    const expected = "21542234 2154333345";
+    expect(actual).to.eql(expected);
+  })
+  it("should decode accurately", () =>{
+    const input = "21542234 2154333345";
+    const encode = false;
+    const actual = polybius(input, encode);
+    const expected = "bugs bunny";
+    expect(actual).to.eql(expected);
+  })
+  it("should ignore caps", () =>{
+    const input = "BUGS BUNNY";
+    const encode = true;
+    const actual = polybius(input, encode);
+    const expected = "21542234 2154333345";
+    expect(actual).to.eql(expected);
+  })
+})
+
