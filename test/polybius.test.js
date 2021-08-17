e6d7b5fb675f6df756153ea57c5a9c48e9e4a54d
Phoenix <phoenixrabang@gmail.com>
2021-08-17 11:34:43 -0700

    second days commit pt2

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,35 +1,42 @@
 // Write your tests here!
 const expect = require("chai").expect;
-const {polybius} = require("../src/polybius");
+const { polybius } = require("../src/polybius");
 
 
 describe("polybius", () => {
-  it("should encode each letter to corresponding number pair", () =>{
+  it("should encode each letter to corresponding number pair", () => {
     const input = "bugs bunny";
     const encode = true;
     const actual = polybius(input, encode);
     const expected = "21542234 2154333345";
     expect(actual).to.eql(expected);
   })
-  it("should output a string", () =>{
+  it("should output a string", () => {
     const input = "bugs bunny";
     const encode = true;
     const actual = polybius(input, encode);
     const expected = "21542234 2154333345";
     expect(actual).to.eql(expected);
   })
-  it("should decode accurately", () =>{
+  it("should decode accurately", () => {
     const input = "21542234 2154333345";
     const encode = false;
     const actual = polybius(input, encode);
     const expected = "bugs bunny";
     expect(actual).to.eql(expected);
   })
-  it("should ignore caps", () =>{
+  it("should ignore caps", () => {
     const input = "BUGS BUNNY";
     const encode = true;
     const actual = polybius(input, encode);
     const expected = "21542234 2154333345";
+    expect(actual).to.eql(expected);
+  })
+  it("should return false if string, excluding spaces, totals to an odd number", () => {
+    const input = "43434"
+    const encode = false
+    const actual = polybius(input, encode)
+    const expected = false
     expect(actual).to.eql(expected);
   })
 })
